67a32344a347862c3af422c2e204a60bda1e9ae9
Kofi B <kofi.boateng@verizon.com>
2025-01-14 01:23:45 -0500

    created config file

---
+++ app/config.py
@@ -0,0 +1,8 @@
+import os
+
+class Config:
+    SECRET_KEY = "secretkey"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///chat.db"
+    GOOGLE_0AUTH_CLIENT_ID = ""
+    GOOGLE_0AUTH_CLIENT_SECRET = ""
+    OAUTHLIB_INSECURE_TRANSPORT = "1"
